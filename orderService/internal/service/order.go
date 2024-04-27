2ea0517904685fd41fc299f244fed9754dc70b03
farsa <puppetcooder@gmail.com>
2024-04-27 19:19:23 +0300

    branch main/ remove empty folder-order service

--- orderService/internal/service/order.go
+++
@@ -1,9 +0,0 @@
-package service
-
-import "order-svc/ports"
-
-type Service struct {
-	userRepository ports.UserRepository
-	eventProducer  ports.EventProducer
-	logger         ports.Logger // New: Logger
-}
--- orderService/ports/rabbitmq.go
+++
@@ -1,7 +0,0 @@
-package ports
-
-import "context"
-
-type EventProducer interface {
-	PublishUserRegisteredEvent(ctx context.Context, userID string) error
-}
