a056f64954d99d5b1609263e3e042343d980b014
farsa <puppetcooder@gmail.com>
2024-05-16 17:25:21 +0300

    branch main/ add shutdown scripts

---
+++ authService/scripts/shutdown_script_authService.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Identify container ID or name for service 1
+container_id=$(docker ps -q --filter name=auth-svc)
+
+# Send SIGTERM signal to service 1 container
+docker kill --signal SIGTERM $container_id
+
+# (Optional) Wait for the server to finish shutting down
+# sleep 10 
+
+# (Optional) Force shutdown if necessary (use with caution)
+# docker kill --signal KILL $container_id
